bb7df0fb63751dcceadea9cb9bc203d8a1c54a25
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-06-11 23:01:05 -0400

    finished

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -1,1 +1,31 @@
 -- Comments in SQL Start with dash-dash --
+--Query 1--
+SELECT * FROM analytics WHERE id = 1880;
+--Query 2--
+SELECT id, app_name FROM analytics WHERE last_updated = 2018-08-01;
+--Query 3--
+SELECT category, COUNT(*) FROM analytics GROUP BY category;
+--Query 4--
+SELECT app_name, reviews FROM analytics ORDER BY reviews DESC LIMIT 5;
+--Query 5--
+SELECT app_name, reviews, rating FROM analytics WHERE rating > 4.8 ORDER BY reviews DESC LIMIT 1;
+--Query 6--
+SELECT category, AVG(rating) FROM analytics GROUP BY category ORDER BY AVG(rating) DESC;
+--Query 7--
+SELECT app_name, price, rating FROM analytics WHERE rating < 3 ORDER BY price DESC LIMIT 1;
+--Query 8--
+SELECT app_name FROM analytics WHERE min_installs <= 50 AND rating > 0 ORDER BY rating DESC;
+--Query 9--
+SELECT app_name FROM analytics WHERE rating < 3 AND reviews > 10000;
+--Query 10--
+SELECT app_name FROM analytics WHERE price BETWEEN 0.10 AND 1.00 ORDER BY reviews DESC LIMIT 10;
+--Query 11--
+SELECT app_name, last_updated FROM analytics ORDER BY last_updated LIMIT 1;
+--Query 12--
+SELECT app_name, price FROM analytics ORDER BY price DESC LIMIT 1;
+--Query 13--
+SELECT SUM(reviews) FROM analytics;
+--Query 14--
+SELECT category, COUNT(*) FROM analytics GROUP BY category HAVING COUNT(*) > 300;
+--Query 15--
+SELECT app_name, reviews, min_installs, min_installs/reviews AS proportion FROM analytics WHERE min_installs >= 100000 ORDER BY proportion DESC LIMIT 1; 
--- queries_products.sql
+++ queries_products.sql
@@ -1,1 +1,29 @@
 -- Comments in SQL Start with dash-dash --
+--Query 1--
+INSERT INTO products (name, price, can_be_returned) VALUES ('chair', 44.00, false);
+--Query 2--
+INSERT INTO products (name, price, can_be_returned) VALUES ('stool', 25.99, true);
+--Query 3--
+INSERT INTO products (name, price, can_be_returned) VALUES ('table', 124.00, false);
+--Query 4--
+SELECT * FROM products;
+--Query 5--
+SELECT name FROM products;
+--Query 6--
+SELECT name, price FROM products;
+--Query 7--
+INSERT INTO products (name, price, can_be_returned) VALUES ('camera', 1200.00, false);
+--Query 8--
+SELECT * FROM products WHERE can_be_returned = true;
+--Query 9--
+SELECT * FROM products WHERE price < 44.00;
+--Query 10--
+SELECT * FROM products WHERE price BETWEEN 22.50 AND 99.99;
+--Query 11--
+UPDATE products SET price = price - 20;
+--Query 12--
+DELETE FROM products WHERE price < 25;
+--Query 13--
+UPDATE products SET price = price + 20;
+--Query 14--
+UPDATE products SET can_be_returned = true;
